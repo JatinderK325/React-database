d012c3020444ffa5cb9e924e2eb9aed56857db32
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-07-15 15:26:59 -0400

    The UI of react-database has been added

---
+++ src/components/MoviesList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Movie from './Movie';
+import classes from './MoviesList.module.css';
+
+const MovieList = (props) => {
+  return (
+    <ul className={classes['movies-list']}>
+      {props.movies.map((movie) => (<Movie
+        title={movie.title}
+        openingText={movie.openingText}
+        releaseDate={movie.releaseDate}
+      />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
