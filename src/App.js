4881cbb34b0fc911437a541779eb4b0ceed89de4
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-07-16 14:43:49 -0400

    try-catch added for handling http errors

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [movies, setMovies] = useState([]);
   // To show loading spinner or loading text, in react its just managing state. we can tell whether waiting or not.
   const [isLoading, setIsLoading] = useState(false);
+  // for handling http errors
+  const [error, setError] = useState(null);
 
   /* const dummyMovies = [
     {
@@ -43,18 +45,29 @@ function App() {
   // when we are dealing with 'promises', we can build 'then' calls. we can also use its alternative syntax. Alternative syntax: using async/await.....
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      // const response2 = await fetch('https://swapi.dev/api/film/');
+      // try response2 with wrong url (film instead of films) to see the result of try-catch block
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
-    });
-    setMovies(transformedMovies);
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      setMovies(transformedMovies);
+    }
+    catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -64,10 +77,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {/* to show content when we are not loading but did not get movies yet, means if movies' array is empty. */}
-        {!isLoading && movies.length === 0 && <p>No movies found!</p>}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found!</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
